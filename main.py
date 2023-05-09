38dc261ba50a3c1d245b12d06faa96c3fa14c652
Pixelum2022 <morale_ouie_0u@icloud.com>
2023-05-09 14:47:18 -0400

    add my_button By.XPATH, works for connecting to site...

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # import modules :
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 
 # Init Variables:
 mon_email = 'harnais_minimal.0@icloud.com'
@@ -15,3 +16,18 @@ chrome_driver_path = "/Applications/chromedriver_mac_arm64/chromedriver"
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get(site_url)
 
+# find field and enter user name:
+my_user_name = driver.find_element(By.ID, 'session_key')
+my_user_name.send_keys(mon_email)
+
+# find field and enter password:
+my_password = driver.find_element(By.ID, 'session_password')
+my_password.send_keys(mon_mp)
+
+# find submit button and click it:
+my_button = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form[1]/div[2]/button')
+my_button.send_keys(Keys.ENTER)
+
+time.sleep(20)
+driver.quit()
+# Ok, fonctionne pour se connecter automatiquement sur LinkedIn
