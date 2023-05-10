3e0ec4ce991700f9eaa84985e33071689ff3cd02
Pixelum2022 <morale_ouie_0u@icloud.com>
2023-05-10 11:03:58 -0400

    created main_V2.py

---
+++ main_V2.py
@@ -0,0 +1,35 @@
+# import modules :
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+# Init Variables:
+mon_email = 'harnais_minimal.0@icloud.com'
+mon_mp = '^&JL3wbJMPh8^LkXfovo'
+site_url = 'https://www.linkedin.com/home'
+# site_url = 'https://www.linkedin.com/jobs/search/?currentJobId=3558537636&f_WT=2&geoId=101330853&keywords=python%20developper&location=Montr%C3%A9al%2C%20Qu%C3%A9bec%2C%20Canada&refresh=true'
+chrome_driver_path = "/Applications/chromedriver_mac_arm64/chromedriver"
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get(site_url)
+
+# login sequence
+# find field and enter user name:
+my_user_name = driver.find_element(By.ID, 'session_key')
+my_user_name.send_keys(mon_email)
+
+# find field and enter password:
+my_password = driver.find_element(By.ID, 'session_password')
+my_password.send_keys(mon_mp)
+
+# find submit button and click it:
+my_button = driver.find_element(By.XPATH, '//*[@id="main-content"]/section[1]/div/div/form[1]/div[2]/button')
+my_button.send_keys(Keys.ENTER)
+
+time.sleep(60)
+driver.quit()
+# Ok, fonctionne pour se connecter automatiquement sur LinkedIn
